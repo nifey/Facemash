61a5ba419bdcf707ef3bd761dae587084f947e6b
Abdun Nihaal <nifey@users.noreply.github.com>
2017-08-06 15:34:52 +0530

    initial layout

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Facesmash</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body onLoad="start();">
+<div id="container">
+	<span id="leftpane" class="pane">
+		<img  id="img1"  src="#" onClick="inc(1)" />	
+	</span>
+
+	<span id="or">	<h2>Or</h2>  </span>
+
+	<span id="rightpane" class="pane">
+		<img id="img2" src="#" onClick="inc(2)" />	
+	</span>
+</div>
+</body>
